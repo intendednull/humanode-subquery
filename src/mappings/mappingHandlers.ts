914c357981c8cfa3f81bb72a8943899d94d329be
Noah <noahcorona231@gmail.com>
2022-09-02 01:41:24 -0700

    Add mapping to auth ticket

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,31 +3,33 @@ import {StarterEntity} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
 
+const BIOAUTH_EXT_METHOD = Uint8Array.from(Buffer.from('0x0400', 'hex'));
+
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
     //Create a new starterEntity with ID using block hash
     let record = new StarterEntity(block.block.header.hash.toString());
     //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+    record.blockNumber = block.block.header.number.toNumber();
+
+    let extrinsics = block.block.extrinsics;
+    for (let i = 0; i < extrinsics.length; i++) {
+        let extrinsic = extrinsics[i];
+        if (extrinsic.method.callIndex === BIOAUTH_EXT_METHOD) {
+            record.signer = extrinsic.signer.toString();
+            record.signature = extrinsic.signature.toString();
+
+
+            record.ticket = extrinsic.method.args['req']['ticket'];
+            record.ticketSignature = extrinsic.method.args['req']['ticketSignature'];
+
+            await record.save();
+        }
+    }
+
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    // Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
+export async function handleEvent(event: SubstrateEvent): Promise<void> {}
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    // Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    // Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
+export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {}
 
 
